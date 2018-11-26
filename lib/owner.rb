e2d522525b63e3b0ac33117c3f6b69b35329ed36
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-26 02:49:24 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,28 @@
 class Owner
-  # code goes here
+
+  attr_accessor :name
+
+  @@owners = []
+  @@ownercount = 0
+
+  def initialize(name)
+    @name = name
+    @@owners << self
+    @@ownercount += 1
+  end
+
+  def self.all
+    @@owners
+  end
+
+  def self.count
+    @@ownercount
+  end
+
+  def self.reset_all
+    @@ownercount * 0
+    @@ownercount
+  end
+
+
 end
